330316fb3c182ec0b4e9e63f4e026d23ede8a375
Vinicyus <vinicyus346@gmail.com>
2022-01-31 12:00:45 -0300

    fix: now only cart with R$ 30,00 will be able to bet

--- app/Controllers/Http/BetsController.ts
+++ app/Controllers/Http/BetsController.ts
@@ -1,18 +1,39 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Bet from 'App/Models/Bet';
+import Game from 'App/Models/Game';
 import DestroyBetValidator from 'App/Validators/Bets/DestroyBetValidator';
 import ShowBetValidator from 'App/Validators/Bets/ShowBetValidator';
 import StoreBetValidator from 'App/Validators/Bets/StoreBetValidator';
+
+interface Ibet {
+  userId: string;
+  gameId: string;
+  value: number;
+}
 
 export default class BetsController {
   public async store({ request, response }: HttpContextContract) {
     await request.validate(StoreBetValidator);
 
-    const { userId, bets } = request.body();
+    const { bets } = request.body();
+
+    let totalValue = 0;
+
+    for await (let bet of bets) {
+      const { price } = await Game.findOrFail(bet.gameId);
+      totalValue += price;
+    }
+
+    if (totalValue < 30) {
+      return response.badRequest({
+        message: 'To make a bet your cart must be at least R$ 30,00',
+        totalValue,
+      });
+    }
 
     await Bet.createMany(bets);
 
-    return response.ok({ userId, bets });
+    return response.created();
   }
 
   public async index() {
