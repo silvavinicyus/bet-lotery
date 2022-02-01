510879ad1ce6236cccfc8b601fa913e4f5614629
Vinicyus <vinicyus346@gmail.com>
2022-02-01 00:55:00 -0300

    feat: sending email to those who haven't bet in the last 30 days

--- app/Tasks/SendEmailWhenNotBetting.ts
+++ app/Tasks/SendEmailWhenNotBetting.ts
@@ -5,7 +5,7 @@ import User from 'App/Models/User';
 export default class SendEmailWhenNotBetting extends BaseTask {
   public static get schedule() {
     // return '0 9 * * *';
-    return '14 21 * * *';
+    return '54 03 * * *';
   }
   /**
    * Set enable use .lock file for block run retry task
@@ -18,18 +18,23 @@ export default class SendEmailWhenNotBetting extends BaseTask {
   public async handle() {
     this.logger.info('Startind schedule');
 
-    const { createdAt } = await User.query().firstOrFail();
+    const users = await User.query().preload('bets');
 
-    console.log(createdAt);
+    const dateNow = new Date().getTime();
+    const daysInMilliseconds = 86400000;
 
-    console.log(
-      await Mail.preview((message) => {
-        message
-          .from('info@example.com')
-          .to('vinicyus346@gmail.com')
-          .subject('Welcome Onboard!')
-          .htmlView('emails/nobet', { name: 'Vinicyus' });
-      })
-    );
+    users.forEach(async (user) => {
+      const diffDays = Math.ceil((dateNow - user.createdAt.toMillis()) / daysInMilliseconds);
+
+      if (user.bets.length === 0 || diffDays > 30) {
+        await Mail.preview((message) => {
+          message
+            .from('info@example.com')
+            .to('vinicyus346@gmail.com')
+            .subject("Let's Bet!")
+            .htmlView('emails/nobet', { name: 'Vinicyus' });
+        });
+      }
+    });
   }
 }
